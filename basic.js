1385da4f7fd182d89b7dbb4b415c876ef3c52dec
younhyong <ychoe3386@gmail.com>
2022-03-08 23:01:30 +0900

    3월 1주차 멘토링 내용

---
+++ basic.js
@@ -0,0 +1,22 @@
+// 1. 출력
+console.log("첫번째 콘솔");
+
+// 2. 변수 상수
+let name ="최윤형";
+console.log(name);
+
+name = "이영교";
+console.log(name);
+
+const age = "나이";
+console.log(age);
+
+// 3. 자료형
+const x = 1;
+const y = "문자열";
+const t = true;
+
+console.log(x, y, t);
+
+console.log(x + 1);
+
